3ead2b7ea75700d61f0f2b8fa5993f4f568f1f01
yangdepp <965093975@qq.com>
2019-01-19 21:51:43 +0800

    fix

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,6 +1,8 @@
 module.exports = (isDev) => {
   return {
     preserveWhitepace: true,
-    extractCSS: true,  //vue中的css单独打包到一个文件
+    extractCSS: !isDev,  //vue中的css单独打包到一个文件
+    cssModules: {},
+    //  hotReload: false,  //对于vue的热重载关闭,根据环境变量生成
   }
 }
